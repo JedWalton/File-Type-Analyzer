20e88061b1c41b8cdea5956f71e55827169f1fce
Jed <jedwalton98@gmail.com>
2022-06-05 16:00:52 +0100

    1/5

--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -2,13 +2,23 @@ package analyzer;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.print(Files
-                .readAllLines(Paths.get(args[0]))
-                .stream()
-                .anyMatch(s -> s.contains(args[1])) ? args[2] : "Unknown file type");
+        String file = args[0];
+        String pattern = args[1];
+        String res = args[2];
+
+        Path path = Paths.get(file);
+        byte[] data = Files.readAllBytes(path);
+
+        String text = new String(data);
+        if (text.contains(pattern)){
+            System.out.println(res);
+        } else {
+            System.out.println("Unknown file type");
+        }
     }
 }
