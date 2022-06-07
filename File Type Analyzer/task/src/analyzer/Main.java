10aa84a6a94e87269ae26a85d4cf02a2caaa5ea5
Jed <jedwalton98@gmail.com>
2022-06-07 03:29:48 +0100

    4/5

--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -3,42 +3,141 @@ package analyzer;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.concurrent.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-//        String flag = args[0];
-//        String flag = "--naive";
-        String folderWithFiles = args[0];
-//        String file = args[1];
-        String pattern = args[1];
-        String fileType = args[2];
-        File folder = new File(folderWithFiles);
 
+    static List<String[]> patterns = new ArrayList<>();
 
-        ExecutorService es = Executors.newCachedThreadPool();
+    public static boolean kmp(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
 
-
-        if (folder.isDirectory()) {
-            File[] files = folder.listFiles();
-            /* New thread for each file in iteration */
-            for (File file : files) {
-                byte[] data = Files.readAllBytes(file.toPath());
-                String text = new String(data);
-                es.execute(new KNP_algorithmThread(text, pattern, fileType, file));
-            }
-            es.shutdown();
-            boolean finished = es.awaitTermination(1, TimeUnit.MINUTES);
+        if(m > n) {
+            return false;
         }
 
-//        String text = new String(data);
-//        if (flag.equals("--naive")) {
-//            naive(text, pattern, fileType);
-//        } else if (flag.equals("--KMP")) {
-//            KNP(text, pattern, fileType);
-//        }
+        // prefix function for pattern
+        int[] pref = new int[m];
+        for(int i = 1; i < m; i++) {
+            int j = pref[i - 1];
+            while(pattern.charAt(i) != pattern.charAt(j) && j != 0) {
+                j = pref[j - 1];
+            }
+            if(pattern.charAt(i) == pattern.charAt(j)) {
+                pref[i] = j + 1;
+            }
+        }
+
+        // check if occurs
+        boolean occurs = false;
+        for(int i = 0, j = 0; i < n; i++) {
+            while(text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+                if(i == n || j == m) break;
+            }
+            if(j == m) {
+                occurs = true;
+                break;
+            }
+
+            if(j != 0) {
+                j = pref[j - 1];
+                i--;
+            }
+        }
+
+        return occurs;
+    }
+
+    static class Worker extends Thread {
+        File file;
+        Path path;
+        String text;
+
+        Worker(File file) {
+            this.file = file;
+            try {
+                path = Paths.get(file.getPath());
+                text = new String(Files.readAllBytes(path));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                String result = String.format("%s: Unknown file type", file.getName());
+
+                for(String[] pattern : patterns) {
+                    if(kmp(text, pattern[1])) {
+                        result = String.format("%s: %s", file.getName(), pattern[2]);
+                        break;
+                    }
+                }
+
+                System.out.println(result);
+
+            } catch(Exception e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        File root = new File(args[0]);
+        File patternsDb = new File(args[1]);
+
+        try {
+            List<String> lines = Files.readAllLines(patternsDb.toPath());
+            for(String pattern : lines) {
+                String[] patternArr = pattern.split(";");
+                patternArr[1] = patternArr[1].replaceAll("\"", "");
+                patternArr[2] = patternArr[2].replaceAll("\"", "");
+                patterns.add(patternArr);
+            }
+
+            patterns.sort(new Comparator<String[]>() {
+                public int compare(String[] first, String[] second) {
+                    return second[0].compareTo(first[0]);
+                }
+            });
+
+//            for(var pattern : patterns) {
+//                System.out.println(pattern[0] + " "
+//                + pattern[1] + " " + pattern[2]);
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File[] files = root.listFiles();
+
+        int poolSize = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(poolSize);
+
+        List<Future<?>> futures = new ArrayList<>();
+
+        for(File file : Objects.requireNonNull(files)) {
+            futures.add(executor.submit(new Worker(file)));
+        }
+
+        for(var future : futures) {
+            try {
+                future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
